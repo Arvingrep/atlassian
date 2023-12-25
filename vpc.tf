73c34b79d70fc07c5d71c749f735086f752a3844
arvin <arvingrep@gmail.com>
2023-12-25 18:48:42 +0800

    update sg 安全组

--- vpc.tf
+++ vpc.tf
@@ -46,6 +46,26 @@ resource "aws_default_security_group" "default-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 13306
+    to_port     = 13306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   ingress {
     from_port   = 7990
     to_port     = 7990
