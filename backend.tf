700e0eabc1a05905e2a39a65802e92d303144b65
arvin <arvingrep@gmail.com>
2023-12-25 15:58:05 +0800

    terraform clound

--- backend.tf
+++ backend.tf
@@ -1,15 +1,8 @@
 terraform {
-  backend "s3" {
-    bucket = "terraform-atlassian"
-    region = "ap-northeast-2"
-    key = "atlassian-server/terraform.tfstate"
+  cloud {
+    organization = "2up"
+    workspaces {
+      name = "atlassian"
+    }
   }
-  required_providers {
-  # We recommend pinning to the specific version of the Docker Provider you're using
-  # since new versions are released frequently
-  docker = {
-    source  = "kreuzwerker/docker"
-    version = "3.0.2"
-  }
-}
 }
